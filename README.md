1db137b3b823bb4503df52228ec30411adc10aa0
mdaquin <mathieu@daquin.net>
2020-07-10 11:45:51 +0100

    added union, difference, toString, and basic tests

--- README.md
+++ README.md
@@ -2,9 +2,14 @@
 
 This is a simple javascript class to represent potentially large  binary vectors as a set of integers, and realise operations such as intersection through basic binary operations for efficiency. This was developped based on the need of using formal concept analysis in javascript efficiently.
 
-Usage is relatively simple: Instanciate the class with an actual binary vector (i.e. an array of booleans) and apply operations such as
+Usage is relatively simple: Instanciate the class with an actual binary vector (i.e. an array of booleans) and apply operations, for example:
 
-'''
-bn3 = bn1.intersection(bn2)
-'''
+```javascript
+var bv1 = new BinaryVector([false,false,true,true,true,true]);
+var bv2 = new BinaryVector([true,false,true,true,false,false]);
+var bv1ibv2 = bv1.intersection(bv2);
+console.log(bv1ubv2.toString());
+```
+
+See test.html for basic test. This was also tested on an FCA framework (TBC) for intersection.
 
--- bvo.js
+++ bvo.js
@@ -29,17 +29,46 @@ class BinaryVector {
 	return re;
     }
     union(bv){
-	// TODO 
+	var re = new BinaryVector([]);
+	re.size = bv.size;
+	var largest = bv;
+	if (this.size > bv.size){
+	    re.size = this.size;
+	    largest = this;
+	}
+	for (var i=0; i < re.size; i++) re._representation.push(largest[i]);
+	for (var i=0; i <  Math.min(this.size, bv.size); i++)
+	    re._representation[i] = this._representation[i]|bv._representation[i];
+	return re;
     }
     difference(bv){
-	// TODO
+	var re = new BinaryVector([]);
+	re.size = Math.max(this.size, bv.size);
+	for (var i=0; i < re.size; i++) re._representation.push(0);
+	for (var i=0; i <  Math.min(this.size, bv.size); i++)
+	    re._representation[i] = this._representation[i] & ~bv._representation[i];
+	return re;	
     }
-    invert(bv){
-	// TODO
+    // this does not work as it also inverts
+    // the padding 0s that are not part of the representaion
+    invert(){
+	var re = new BinaryVector([]);
+	re.size = this.size;
+	for (var i in this._representation){
+	    re._representation.push(~this._representation[i]);
+	}
+	return re;
     }
     get(i){
 	var i1 = Math.floor(i/32);
         var i2 = i - i1*32;
         return ((this._representation[i1] & (1 << i2)) != 0);
+    }
+    toString(){
+	var re = "";
+	for (var i in this._representation){
+	    re += this._representation[i].toString(2).padStart(32, 0).split("").reverse().join("");
+	}
+	return re;
     }
 }
