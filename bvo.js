e399aeabe21c66957e81f126b9d70ee6d8a67ed6
Sylvain Nadeau <sylvain.nadeau@nexio.com>
2021-03-20 00:35:11 -0400

    Uses JavaScript BigInt instead of arrays for performance

--- bvo.js
+++ bvo.js
@@ -1,74 +1,50 @@
-
-
+var zero = BigInt(0)
+var one = BigInt(1)
+var two = BigInt(2)
 class BinaryVector {
     // create the binary vector from an array of booleans
-    constructor(bv){
-	this._representation = []
-	this.size = Math.ceil(bv.length/32.0);
-        for (var i = 0; i < this.size; i++) this._representation.push(0);
-        for (var i in bv){
-	    if (bv[i]){
-                var i1 = Math.floor(i/32);
-                var i2 = i - i1*32;
-	        this._representation[i1] = this._representation[i1] | 1 << i2;
-            }
-        }	
-    }
-    equals(bv){
-	if (bv.size!=this.size) return false;
-	for(var i in this._representation)
-	    if (this._representation[i] != bv._representation[i]) return false;
-	return true;
+	// NOTE: It is assumed that all bv sizes to be equal
+	constructor(v,s=0){
+		if (s){
+			this.rep = v
+			this.size = s
+		} else if (v) {
+			this.rep = zero
+			this.size = v.length
+			for (var i = 0; i < this.size; i++) 
+				this.rep += v[i] ? two ** BigInt(i) : zero	
+		} else {
+			this.rep = zero
+			this.size = 0;
+		}
+	}
+    equals(bv){ // returns boolean
+		return this.rep==bv.rep
     }
     intersection(bv){
-	var re = new BinaryVector([]);
-	re.size = Math.max(this.size, bv.size);
-	for (var i=0; i < re.size; i++) re._representation.push(0);
-	for (var i=0; i <  Math.min(this.size, bv.size); i++)
-	    re._representation[i] = this._representation[i]&bv._representation[i];
-	return re;
+		return new BinaryVector(this.rep&bv.rep, Math.max(this.size,bv.size))
     }
     union(bv){
-	var re = new BinaryVector([]);
-	re.size = bv.size;
-	var largest = bv;
-	if (this.size > bv.size){
-	    re.size = this.size;
-	    largest = this;
-	}
-	for (var i=0; i < re.size; i++) re._representation.push(largest[i]);
-	for (var i=0; i <  Math.min(this.size, bv.size); i++)
-	    re._representation[i] = this._representation[i]|bv._representation[i];
-	return re;
+		return new BinaryVector(this.rep|bv.rep, Math.max(this.size,bv.size))
     }
+	inverse(){
+		return new BinaryVector(two ** BigInt(this.size) - one - this.rep, this.size)
+	}
+	// Not really the difference
+	// Returns true when A=true and B=false
+	// This is the inverse of the implication ~(A->B)
     difference(bv){
-	var re = new BinaryVector([]);
-	re.size = Math.max(this.size, bv.size);
-	for (var i=0; i < re.size; i++) re._representation.push(0);
-	for (var i=0; i <  Math.min(this.size, bv.size); i++)
-	    re._representation[i] = this._representation[i] & ~bv._representation[i];
-	return re;	
+		return this.intersection(bv.inverse())
     }
-    // this does not work as it also inverts
-    // the padding 0s that are not part of the representaion
-    invert(){
-	var re = new BinaryVector([]);
-	re.size = this.size;
-	for (var i in this._representation){
-	    re._representation.push(~this._representation[i]);
-	}
-	return re;
-    }
-    get(i){
-	var i1 = Math.floor(i/32);
-        var i2 = i - i1*32;
-        return ((this._representation[i1] & (1 << i2)) != 0);
+    get(i){ // return boolean
+		var bigi = two ** BigInt(i)
+		return this.rep >= bigi && this.rep / bigi % two == 1
     }
     toString(){
-	var re = "";
-	for (var i in this._representation){
-	    re += this._representation[i].toString(2).padStart(32, 0).split("").reverse().join("");
-	}
-	return re;
+		return this.rep.toString()
+		/*var re = " ";
+		for (var i = 0; i < this.size; i++)
+			re += this.get(i) + " "
+		return re;*/
     }
 }
